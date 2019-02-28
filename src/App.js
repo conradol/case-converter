a71c4f070bf4d41c8a03b2695804eb116fd9c56c
Conrado Lemos <conrado.lemos@hotmail.com>
2019-02-27 22:02:54 -0300

    Case converter in React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const lower = "lower"
+const upper = "upper"
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { message: ""}
+  }
+
+  converter(action) {
+
+    let message = this.state.message
+
+    switch (action) {
+      case lower:
+        message = message.toLowerCase()
+        break
+      
+      case upper:
+        message = message.toUpperCase()
+        break
+      
+      default:
+        break
+    }
+
+    this.setState({ message })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <textarea onChange={(evt) => {
+          let message = evt.target.value
+          this.setState({message})
+        }} className="textarea" value={this.state.message}>
+          Texto Qualquer
+        </textarea>
+        <div>
+          <button onClick={() => this.converter(lower)}>lowercase</button>
+          <button onClick={() => this.converter(upper)}>UPPERCASE</button>
+        </div>
       </div>
     );
   }
